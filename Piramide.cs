98d509298c9f6ce8289ffbc6ea8d71e7f106f424
KatEscaner <jjmateo1279@gmail.com>
2022-10-03 17:42:08 +0200

    add Exception to Piramide.cs

--- Piramide.cs
+++ Piramide.cs
@@ -1,33 +1,46 @@
+using System;
+
 class Piramide{
 	static void Main(){
 		int num;
-		
-		System.Console.Write("Introduzca un número impar: ");
-		num = System.Convert.ToInt32(System.Console.ReadLine());
-		System.Console.WriteLine();
-
-			if(num%2 != 0){
-			for(int i=(num-1)/2; i>0; i--)
-				System.Console.Write(" ");
-			
-			System.Console.Write("*");
+		try{
+			System.Console.Write("Introduzca un número impar: ");
+			num = System.Convert.ToInt32(System.Console.ReadLine());
 			System.Console.WriteLine();
 
-			int count = num-2;
-
-			while(count>0){
-				for(int i=2; i<count; i+=2)
+				if(num%2 != 0){
+				for(int i=(num-1)/2; i>0; i--)
 					System.Console.Write(" ");
 				
-				for(int j=num+1; j>count; j--)
-					System.Console.Write("*");
-				
-					
+				System.Console.Write("*");
 				System.Console.WriteLine();
-				count-=2;
-			}	
-		} else {
-			System.Console.WriteLine("El numero introducido es par, tiene que ser impar");
+
+				int count = num-2;
+
+				while(count>0){
+					for(int i=2; i<count; i+=2)
+						System.Console.Write(" ");
+					
+					for(int j=num+1; j>count; j--)
+						System.Console.Write("*");
+					
+						
+					System.Console.WriteLine();
+					count-=2;
+				}	
+			} else {
+				System.Console.WriteLine("El numero introducido es par, tiene que ser impar");
+			}
+		} catch(Exception e){
+			if(e.GetType().Name == "FormatException"){
+					System.Console.WriteLine("Formato incorrecto");
+					System.Console.WriteLine();
+					System.Console.WriteLine();
+
+				} else {
+					System.Console.WriteLine(e.GetType().Name);
+					System.Console.WriteLine();
+				}
 		}
 	}
 }
